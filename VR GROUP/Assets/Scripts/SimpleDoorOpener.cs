d927840ead99eff9d3896d5ae92ab65b1c200880
Banana Cat <142626457+Kennynom@users.noreply.github.com>
2025-05-28 23:55:41 +0800

    Update Sound + Builds are included here

--- VR GROUP/Assets/Scripts/SimpleDoorOpener.cs
+++ VR GROUP/Assets/Scripts/SimpleDoorOpener.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class SimpleDoorOpener : MonoBehaviour
 {
-    public Vector3 openPosition = new Vector3(-21.3339996f, -2.01999998f, -4.04799986f);
+    public Vector3 openPosition = new Vector3(-21.3339996f, -2.01999998f, -4.16f);
     public Vector3 openRotationEuler = Vector3.zero; // set in inspector
     private Vector3 closedPosition;
     private Quaternion closedRotation;
